b0d673bdf9cc536626ecffc07589844df141efca
clary <wcl58981298@gmail.com>
2015-02-22 15:18:58 +0800

    route repair

--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,19 @@ var markdown = require('markdown-js');
 var root_views = __dirname + "/../views"
 
 module.exports = function(app){
-	// app.use(function(req, res, next) {
-	//     var err = new Error('Not Found');
-	//     err.status = 404;
-	//     next(err);
-	// });
-	
-	app.get("*", function(req, res){
-		console.log( path.existsSync(root_views+req.url) );
+	app.get("^/[^/]*/$", function(req, res){
+		console.log(123123);
 		if( path.existsSync(root_views+req.url) ){
 			fs.writeFileSync( path.join(root_views+req.url+"index.html")
 				, markdown.makeHtml( fs.readFileSync(path.join(root_views+req.url+"index.md"),"utf-8") )
 			);
 			console.log( path.join(req.url+"index").substr(1) );
 			res.render( path.join(req.url+"index").substr(1),{});
-			// res.send( req.url );
 		} else {
-			res.send("safasdf");
+			res.send("Can I help you?");
 		}
 	})
-
-
-	app.get('/md5/', function(req, res){
-		res.send( crypto.createHash('md5').update( req.query.password ).digest('base64') );
+	app.get("*", function(req, res){
+		res.send("Can I help you?");
 	});
-	//app.get("*", route_err.http404 );
-
 }
