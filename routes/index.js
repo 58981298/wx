b50aab2428f31d8d70c32e8f98bd147428c45278
clary <wcl58981298@gmail.com>
2015-02-22 15:33:27 +0800

    repair route

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,10 @@ var root_views = __dirname + "/../views"
 
 module.exports = function(app){
 	app.get("^/[^/]*/$", function(req, res){
-		if( path.existsSync(root_views+req.url) ){
+		if( fs.existsSync( path.join(root_views+req.url+"/index.md") )){
 			fs.writeFileSync( path.join(root_views+req.url+"index.html")
 				, markdown.makeHtml( fs.readFileSync(path.join(root_views+req.url+"index.md"),"utf-8") )
 			);
-			console.log( path.join(req.url+"index").substr(1) );
 			res.render( path.join(req.url+"index").substr(1),{});
 		} else {
 			res.send("Can I help you?");
